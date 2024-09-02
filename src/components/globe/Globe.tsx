633454cf66f896da9e8aecb22834203001c49857
Ben Gladman <ben.gladman@gmail.com>
2024-09-02 14:14:12 +0100

    Reset x rotation when no trip selected

--- src/components/globe/Globe.tsx
+++ src/components/globe/Globe.tsx
@@ -1,34 +1,41 @@
 import { useFrame } from '@react-three/fiber';
 import { BlueSphere } from './BlueSphere';
 import { Land } from './Land';
-import { useMemo, useRef } from 'react';
+import { useEffect, useMemo, useRef } from 'react';
 import { Euler, Group, MathUtils, Quaternion } from 'three';
 import { useTripStore } from '../../state/tripStore';
 
 export function Globe() {
   const groupRef = useRef<Group>(null!);
-  const selectedTrip = useTripStore((s) => s.selectedTrip);
 
-  const targetQuaternion = useMemo(() => {
-    if (selectedTrip?.coordinates.length) {
-      const euler = new Euler(
-        MathUtils.degToRad(selectedTrip.coordinates[0][0]),
-        MathUtils.degToRad(270 - selectedTrip.coordinates[0][1]),
+  const selectedCoordinates = useTripStore(
+    (s) => s.selectedTrip?.coordinates[0],
+  );
+
+  const targetQuaternion = useMemo(() => new Quaternion(), []);
+  const targetEuler = useMemo(() => new Euler(), []);
+
+  useEffect(() => {
+    if (selectedCoordinates) {
+      targetEuler.set(
+        MathUtils.degToRad(selectedCoordinates[0]),
+        MathUtils.degToRad(270 - selectedCoordinates[1]),
         0,
       );
 
-      return new Quaternion().setFromEuler(euler);
+      targetQuaternion.setFromEuler(targetEuler);
+    } else {
+      targetEuler.x = 0;
     }
-
-    return null;
-  }, [selectedTrip]);
+  }, [selectedCoordinates, targetQuaternion, targetEuler]);
 
   useFrame((_, delta) => {
-    if (targetQuaternion) {
-      groupRef.current.quaternion.slerp(targetQuaternion, delta);
-    } else {
-      groupRef.current.rotation.y -= delta / 4;
+    if (!selectedCoordinates) {
+      targetEuler.y -= delta / 4;
+      targetQuaternion.setFromEuler(targetEuler);
     }
+
+    groupRef.current.quaternion.slerp(targetQuaternion, delta);
   });
 
   return (
